c547dfe941f2262c8b078f85a7bd7277cec6c3ca
vishusharmaX <sharmavishwajeet04@gmail.com>
2023-02-07 00:57:10 +0530

    initial

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,13 +4,13 @@ import {AiOutlineInstagram,AiOutlineGithub,AiOutlineLinkedin} from 'react-icons/
 const Footer= ()=>(
     <Box  textAlign={"center"} p="5" color={"gray.600"} borderTop="1px" borderColor={'gray.400'}>
         <Flex flexWrap={"wrap"} justifyContent="space-evenly" alignItems={"center"} padding="5px" fontSize="40px" mb="14px">
-            <Link href={"https://www.instagram.com/vishu.livincool/"}>
+            <Link href={"https://www.instagram.com/vishu.livincool/"} passHref>
                 <AiOutlineInstagram/>
             </Link>
-            <Link href={"https://github.com/vishusharmaX"}>
+            <Link href={"https://github.com/vishusharmaX"} passHref>
                 <AiOutlineGithub/>
             </Link>
-            <Link href={"https://www.linkedin.com/in/vishwajeet-sharma-649a21205/"}>
+            <Link href={"https://www.linkedin.com/in/vishwajeet-sharma-649a21205/"} passHref>
                 <AiOutlineLinkedin/>
             </Link>
         </Flex>
